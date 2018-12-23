f93a4dde29a4e3460fd24124a8e83e358fca5c27
yogeshwar <yo1ge4sh3@gmail.com>
2018-12-23 14:36:01 +0530

    yogy

---
+++ src/main/java/gitnewdemobyme/gitdemo1.java
@@ -0,0 +1,11 @@
+package gitnewdemobyme;
+
+public class gitdemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git demo now");
+
+	}
+
+}
